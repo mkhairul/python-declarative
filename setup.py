c3e5682e3707f9f818e248156edbb1bb0fefdb47
mkhairulhr <103471814+mkhairulhr@users.noreply.github.com>
2023-12-28 15:58:51 +0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python_declarative",
+    version="0.1.0",
+    description="A simple Python package for creating declarative style system",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">3.6"
+)
